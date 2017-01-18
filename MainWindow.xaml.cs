4913a3d433101ce38aa31078c7ccb0ca2ff94ce8
lyn826 <zephyr0826@gmail.com>
2017-01-18 11:59:51 +0900

    new
    face 2 face Verification

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.IO;
+
+namespace FaceApiDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("");
+        bool hasFace1 = false;
+        bool hasFace2 = false;
+        Guid faceID1;
+        Guid faceID2;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openDlg = new Microsoft.Win32.OpenFileDialog();
+
+            openDlg.Filter = "Images|*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG;*.TIFF";
+            bool? result = openDlg.ShowDialog(this);
+
+            if (!(bool)result)
+            {
+                return;
+            }
+
+            string filePath = openDlg.FileName;
+
+            Uri fileUri = new Uri(filePath);
+            BitmapImage bitmapSource = new BitmapImage();
+
+            bitmapSource.BeginInit();
+            bitmapSource.CacheOption = BitmapCacheOption.None;
+            bitmapSource.UriSource = fileUri;
+            bitmapSource.EndInit();
+
+            FacePhoto1.Source = bitmapSource;
+
+            label1.Content = "Detecting...";
+            var faces = await UploadAndDetectFaces(filePath);
+            var faceRectangles = faces.Select(face => face.FaceRectangle);
+            var faceRects = faceRectangles.ToArray();
+            label1.Content = String.Format("Detection Finished. {0} face(s) detected", faceRects.Length);
+
+            if (faceRects.Length > 0)
+            {
+                faceID1 = faces[0].FaceId;
+                hasFace1 = true;
+                DrawingVisual visual = new DrawingVisual();
+                DrawingContext drawingContext = visual.RenderOpen();
+                drawingContext.DrawImage(bitmapSource,
+                    new Rect(0, 0, bitmapSource.Width, bitmapSource.Height));
+                double dpi = bitmapSource.DpiX;
+                double resizeFactor = 96 / dpi;
+
+                foreach (var faceRect in faceRects)
+                {
+                    drawingContext.DrawRectangle(
+                        Brushes.Transparent,
+                        new Pen(Brushes.Red, 2),
+                        new Rect(
+                            faceRect.Left * resizeFactor,
+                            faceRect.Top * resizeFactor,
+                            faceRect.Width * resizeFactor,
+                            faceRect.Height * resizeFactor
+                            )
+                    );
+                }
+
+                drawingContext.Close();
+                RenderTargetBitmap faceWithRectBitmap = new RenderTargetBitmap(
+                    (int)(bitmapSource.PixelWidth * resizeFactor),
+                    (int)(bitmapSource.PixelHeight * resizeFactor),
+                    96,
+                    96,
+                    PixelFormats.Pbgra32);
+
+                faceWithRectBitmap.Render(visual);
+                FacePhoto1.Source = faceWithRectBitmap;
+            }
+            if (hasFace1 && hasFace2)
+            {
+                VerifyAsync(faceID1, faceID2);
+            }
+        }
+
+        private async void BrowseButton2_Click(object sender, RoutedEventArgs e)
+        {
+            var openDlg = new Microsoft.Win32.OpenFileDialog();
+
+            openDlg.Filter = "Images|*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG;*.TIFF";
+            bool? result = openDlg.ShowDialog(this);
+
+            if (!(bool)result)
+            {
+                return;
+            }
+
+            string filePath = openDlg.FileName;
+
+            Uri fileUri = new Uri(filePath);
+            BitmapImage bitmapSource = new BitmapImage();
+
+            bitmapSource.BeginInit();
+            bitmapSource.CacheOption = BitmapCacheOption.None;
+            bitmapSource.UriSource = fileUri;
+            bitmapSource.EndInit();
+
+            FacePhoto2.Source = bitmapSource;
+
+            label2.Content = "Detecting...";
+            var faces = await UploadAndDetectFaces(filePath);
+            var faceRectangles = faces.Select(face => face.FaceRectangle);
+            var faceRects = faceRectangles.ToArray();
+            label2.Content = String.Format("Detection Finished. {0} face(s) detected", faceRects.Length);
+
+            if (faceRects.Length > 0)
+            {
+                faceID2 = faces[0].FaceId;
+                hasFace2 = true;
+                DrawingVisual visual = new DrawingVisual();
+                DrawingContext drawingContext = visual.RenderOpen();
+                drawingContext.DrawImage(bitmapSource,
+                    new Rect(0, 0, bitmapSource.Width, bitmapSource.Height));
+                double dpi = bitmapSource.DpiX;
+                double resizeFactor = 96 / dpi;
+
+                foreach (var faceRect in faceRects)
+                {
+                    drawingContext.DrawRectangle(
+                        Brushes.Transparent,
+                        new Pen(Brushes.Red, 2),
+                        new Rect(
+                            faceRect.Left * resizeFactor,
+                            faceRect.Top * resizeFactor,
+                            faceRect.Width * resizeFactor,
+                            faceRect.Height * resizeFactor
+                            )
+                    );
+                }
+
+                drawingContext.Close();
+                RenderTargetBitmap faceWithRectBitmap = new RenderTargetBitmap(
+                    (int)(bitmapSource.PixelWidth * resizeFactor),
+                    (int)(bitmapSource.PixelHeight * resizeFactor),
+                    96,
+                    96,
+                    PixelFormats.Pbgra32);
+
+                faceWithRectBitmap.Render(visual);
+                FacePhoto2.Source = faceWithRectBitmap;
+            }
+
+            if (hasFace1 && hasFace2)
+            {
+                VerifyAsync(faceID1, faceID2);
+            }
+        }
+
+
+        private async Task<Face[]> UploadAndDetectFaces(string imageFilePath)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(imageFilePath))
+                {
+                    var faces = await faceServiceClient.DetectAsync(imageFileStream);
+                    return faces;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private async void VerifyAsync(Guid faceId1, Guid faceId2)
+        {
+            try
+            {
+                var output = "";
+                var result = await faceServiceClient.VerifyAsync(faceId1, faceId2);
+                if (result.Confidence > 0.6)
+                {
+                    output = "Same person!! With " + result.Confidence.ToString() + " Confidence";
+                }
+                else
+                {
+                    output = "NOT Same person!! With " + result.Confidence.ToString() + " Confidence";
+                }
+                lab_result.Content = output;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("SMT is wrong here!");
+            }
+        }
+    }
+}
